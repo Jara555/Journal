510aa6f979e3ad514215bf16dc953d8dfa899700
Jara555 <jaralinders@gmail.com>
2018-05-01 14:52:53 +0200

    Initial layout

--- app/src/main/java/com/jara/journal/MainActivity.java
+++ app/src/main/java/com/jara/journal/MainActivity.java
@@ -1,7 +1,10 @@
 package com.jara.journal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EntryDatabase db = EntryDatabase.getInstance(getApplicationContext());
+
     }
+
+    public void addInput(View view) {
+
+        // create intent and pass clicked friend through to ProfileActivity
+        Intent intent = new Intent(MainActivity.this, InputActivity.class);
+        startActivity(intent);
+    }
+
+    private class OnItemClickListener implements AdapterView.OnItemLongClickListener {
+
+        @Override
+        public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+            return true;
+
+        }
+    }
+
+
 }
